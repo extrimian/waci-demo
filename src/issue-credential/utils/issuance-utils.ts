ce34f09be2b81726529acdf4e03e628cb0a38e3b
szavalia <szavalia@itba.edu.ar>
2023-04-19 11:58:31 -0300

    Added utils to avoid circular imports

---
+++ src/issue-credential/utils/issuance-utils.ts
@@ -0,0 +1,9 @@
+export enum IssuanceMessageTypes {
+  OobInvitation = 'https://didcomm.org/out-of-band/2.0/invitation',
+  ProposeCredential = 'https://didcomm.org/issue-credential/3.0/propose-credential',
+  OfferCredential = 'https://didcomm.org/issue-credential/3.0/offer-credential',
+  RequestCredential = 'https://didcomm.org/issue-credential/3.0/request-credential',
+  IssueCredential = 'https://didcomm.org/issue-credential/3.0/issue-credential',
+  Ack = 'https://didcomm.org/issue-credential/3.0/ack',
+  ProblemReport = 'https://didcomm.org/report-problem/2.0/problem-report',
+}
